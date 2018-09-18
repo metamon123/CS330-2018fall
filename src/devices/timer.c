19c09c5319a6c5987a56cdef5572ccbb35398a36
Dongpyeong Seo <bludrimjr@gmail.com>
2018-09-18 20:39:10 +0900

    [Alarm clock] minor change

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -3,6 +3,7 @@
 #include <inttypes.h>
 #include <round.h>
 #include <stdio.h>
+#include <list.h>
 #include "threads/interrupt.h"
 #include "threads/io.h"
 #include "threads/synch.h"
@@ -24,17 +25,26 @@ static int64_t ticks;
    Initialized by timer_calibrate(). */
 static unsigned loops_per_tick;
 
+static struct list sleep_list;
+
 static intr_handler_func timer_interrupt;
 static bool too_many_loops (unsigned loops);
 static void busy_wait (int64_t loops);
 static void real_time_sleep (int64_t num, int32_t denom);
 
+bool
+less (const struct list_elem *e1, const struct list_elem *e2, void *aux)
+{
+  
+}
 /* Sets up the 8254 Programmable Interval Timer (PIT) to
    interrupt PIT_FREQ times per second, and registers the
    corresponding interrupt. */
 void
 timer_init (void) 
 {
+
+  list_init (&sleep_list);
   /* 8254 input frequency divided by TIMER_FREQ, rounded to
      nearest. */
   uint16_t count = (1193180 + TIMER_FREQ / 2) / TIMER_FREQ;
@@ -97,10 +107,10 @@ void
 timer_sleep (int64_t ticks) 
 {
   int64_t start = timer_ticks ();
+  struct thread *cur = thread_current ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  list_push_back (&sleep_list, &cur->elem);
 }
 
 /* Suspends execution for approximately MS milliseconds. */
@@ -135,7 +145,12 @@ timer_print_stats (void)
 static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
+  /*
   ticks++;
+  thread_tick ();
+  */
+  ticks++;
+
   thread_tick ();
 }
 
